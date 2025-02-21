ea0cb5cbb6509768e20a77b8cdc03f06395ebdf6
Mateo Sotelo <msjulian774gmail.com>
2025-02-21 14:29:16 -0500

    Proyecto java

---
+++ Factura.java
@@ -0,0 +1,47 @@
+public class Factura
+{
+    private int identificacionDeFactura;
+    private Producto productos();  
+
+    public class Factura()
+    {
+
+    } 
+
+    public class Factura(int identificacionDeFactura, Producto productos)
+    {
+        this.identificacionDeFactura=identificacionDeFactura;
+        this.producto=producto;
+    }
+
+
+    public int getIdentificacionDeFactura()
+    {
+        return identificacionDeFactura;
+    }
+    public void setIdentificcionDeFactura(int identificacionDeFactura)
+    {
+        this.identificacionDeFactura=identificacionDeFactura;
+    }
+
+    public Producto getProductos()
+    {
+        return productos;
+    }
+
+    public void setProductos(Producto productos)
+    {
+        this.productos=productos
+    }
+
+
+
+
+
+
+}
+
+Public class Productos
+{
+
+}
